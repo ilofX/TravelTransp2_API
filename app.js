e45f9451a28e5004747bcf119af3e3dfd9ca0a7c
Filippo Stella <filippo.stella.360@gmail.com>
2021-03-10 20:54:54 +0100

    Started gathering documentation for authorization and authentication mechanisms with JWT and plain login

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let helmet = require('helmet');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let resourcesRouter = require('./routes/resources');
+let authenticationRouter = require('./routes/authentication');
+let adminRouter = require('./routes/admin');
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,10 +19,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/auth/', authenticationRouter);
+app.use('/api/res/', resourcesRouter);
+app.use('/api/admin/', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
