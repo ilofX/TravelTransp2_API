423f566459e78b1837eb55b29a5236e3b3984dff
Filippo Stella <filippo.stella.360@gmail.com>
2021-09-23 18:58:28 +0200

    Upgraded Packages and Started implementing JWT and refresh token authentication strategy
    Signed-off-by: Filippo Stella <filippo.stella.360@gmail.com>

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
-let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 let helmet = require('helmet');
+let spdy = require('spdy');
+let Sequelize = require('sequelize');
 
+let config = require('/config/config.json');
 let resourcesRouter = require('./routes/resources');
 let authenticationRouter = require('./routes/authentication');
 let adminRouter = require('./routes/admin');
@@ -17,17 +19,29 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({extended: false}));
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
+
+const sequelize = new Sequelize(config.Database, config.DatabaseUsername, config.DatabasePassword, {
+  host: config.DatabaseAddress + ':' + config.DatabasePort,
+  dialect: config.DatabaseType
+});
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection to the database has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 app.use('/api/auth/', authenticationRouter);
 app.use('/api/res/', resourcesRouter);
 app.use('/api/admin/', adminRouter);
 
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,44 +1,19 @@
 let express = require('express');
 let router = express.Router();
 
-let argon2 = require('argon2');
+let bcrypt = require('bcrypt');
 let jwt = require('jsonwebtoken');
 
-const config = require('../config/config');
-
-
-/** Resources
- *  https://softwareontheroad.com/nodejs-jwt-authentication-oauth/#signup
- *  https://codeforgeek.com/refresh-token-jwt-nodejs-authentication/
- *  https://medium.com/@sugandhasaxena1212/access-token-and-refresh-token-with-node-js-a501e1cc034b
- */
-
-
+const config = require('../config/config.json');
 
 
 router.post('/login', (req,res) => {
-  const postData = req.body;
 
-  // do the database authentication here, with user name and password combination and verify password with argon2.
-
-  const user = {
-    "id": postData.id,
-    "username": postData.username,
-    "mail": postData.mail
-  }
-  const token = jwt.sign(user, config.tokenSecret, { expiresIn: config.tokenLife});
-  const refreshToken = jwt.sign(user, config.refreshTokenSecret, { expiresIn: config.refreshTokenLife});
-  const response = {
-    "status": true,
-    "token": token,
-    "refreshToken": refreshToken,
-  }
-
-  //tokenList[refreshToken] = response Insert token data store
-
-  res.status(200).json(response);
 });
 
+router.post('/refersh', (req, res) => {
+
+});
 
 
 module.exports = router;
